f06cad00a7d5b3ef7a54cc70c27c76bc1ec00157
Brad Gibb <gorathon@gmail.com>
2024-12-04 20:03:45 +0000

    part2-uncomplete

--- day4/2/main.py
+++ day4/2/main.py
@@ -1,54 +1,41 @@
 import re
 
 def main():
-    with open("input_test.txt", 'r') as file:
+    with open("input.txt", 'r') as file:
         lines = file.read().splitlines()
 
-    #create a list of strings for the vertical lines
-    vertical_lines = [""] * len(lines[0])
-    diagonal_lines_1 = [""] * (len(lines)*2 -1)
-    diagonal_lines_2 = [""] * (len(lines)*2 -1)
-    for line in lines:
-        for i, char in enumerate(line):
-            vertical_lines[i] += char
+    x_count = 0
 
+    #foreach line find each 'A'
     for i in range(len(lines)):
-        for j in range(i + 1):
-            diagonal_lines_1[i] += lines[i - j][j]
-            diagonal_lines_2[i] += lines[len(lines) - 1 - i + j][j]
-            if(i != len(lines) - 1):
-                diagonal_lines_1[len(diagonal_lines_1) -1 - i] += lines[len(lines[0]) - 1 - j][len(lines) - 1 - i + j]
-                diagonal_lines_2[len(diagonal_lines_2) -1 - i] += lines[j][len(lines) - 1 - i + j]
+        if(i > 0 and i < len(lines)-1):
+            matches = re.finditer('A', lines[i])
+            for m in matches:
+                if(m.start(0) > 0 and m.start(0) < (len(lines[i])-2)):
+                    l = [False] * 4;
+                    #print(i, m.start(0), len(lines[i])-2)
+                    l[0] = lines[i -1][m.start(0) -1] == 'M' and lines[i +1][m.start(0) +1] == 'S'
+                    l[1] = lines[i -1][m.start(0) +1] == 'M' and lines[i +1][m.start(0) -1] == 'S'
+                    l[2] = lines[i +1][m.start(0) -1] == 'M' and lines[i -1][m.start(0) +1] == 'S'
+                    l[3] = lines[i +1][m.start(0) +1] == 'M' and lines[i -1][m.start(0) -1] == 'S'                    
 
-    pattern = r'(MAS)'
 
-    horizontal_sum = 0
-    for i in range(len(lines)):
-        matches = re.findall(pattern, lines[i])
-        matches = re.finditer(pattern, lines[i])
-        for m in matches:
-            print(m.start(0), m.end(0))
+                    if(l[0] + l[1] + l[2] + l[3] >= 1 and l[0] + l[1] + l[2] + l[3] < 2):
+                        print(i, ' ', m.start(0))
+                        print(lines[i-1][m.start(0) -1],'.', lines[i-1][m.start(0) +1])
+                        print('.', lines[i][m.start(0)], '.')
+                        print(lines[i+1][m.start(0) -1],'.', lines[i+1][m.start(0) +1])
+                        print('')
 
-        #print(str(len(matches)) + " matches for horizontal line " + str(i))
-        #print(lines[i])
-        #horizontal_sum += len(matches)
-        #check the reverse
-        matches = re.findall(pattern, lines[i][::-1])
-        horizontal_sum += len(matches)
-    print(horizontal_sum)
+                    #check if 2 of the 4 above are true
+                    if(l[0] + l[1] + l[2] + l[3] >= 2):
+                        x_count += 1
+                        
 
-    diagonal_sum_1 = 0
-    for i in range(len(diagonal_lines_1)):
-        matches = re.findall(pattern, diagonal_lines_1[i])
-        #print(str(len(matches)) + " matches for diagonal line 1 " + str(i))
-        #print(diagonal_lines_1[i])
-        diagonal_sum_1 += len(matches)
-        #check the reverse
-        matches = re.findall(pattern, diagonal_lines_1[i][::-1])
-        diagonal_sum_1 += len(matches)
-    #print(diagonal_sum_1)
+    print(x_count)
 
-    print(horizontal_sum + diagonal_sum_1)
+
+    
 
 if __name__ == "__main__":
     main()
